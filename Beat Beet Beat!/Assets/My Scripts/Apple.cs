6ed5df77e90c3ad36b848111581f5acb3c7a1aeb
Harrison John Hopkins <hguyhopkins@gmail.com>
2016-08-27 09:11:12 -0400

    Changed Fly to Animated Sprite
    The FLY Enemy now has an animated sprite.

---
+++ Beat Beet Beat!/Assets/My Scripts/Apple.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple : Enemy
+{
+    public GameObject player;
+
+    public bool chasing = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+	}
+
+    public override void Move()
+    {
+        if(chasing)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x, 0.1f), 0.02f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            chasing = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            chasing = false;
+        }
+    }
+}
