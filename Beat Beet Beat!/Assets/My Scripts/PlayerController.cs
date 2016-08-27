487ebfd9fbf57f8b35b6f3b631013a61a9695431
Harrison John Hopkins <hguyhopkins@gmail.com>
2016-08-27 17:03:56 -0400

    Added Brad Beet
    Replaced place-holder art for player with Brad Beet

--- Beat Beet Beat!/Assets/My Scripts/PlayerController.cs
+++ Beat Beet Beat!/Assets/My Scripts/PlayerController.cs
@@ -1,28 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float speed;
 
     private Rigidbody2D rb;
     private bool isJumping = false;
 
+    private Animator anim;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         rb = GetComponent<Rigidbody2D>();
 
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         
         float hor = Input.GetAxisRaw("Horizontal");
         if (!isJumping && GetComponent<Rigidbody2D>().velocity.y < 1 && Input.GetAxisRaw("Vertical") > 0)
         { 
-            GetComponent<Rigidbody2D>().AddForce(transform.up * 750);
+            GetComponent<Rigidbody2D>().AddForce(transform.up * 250);
             isJumping = true;
         }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) //left
+        {
+            anim.SetInteger("isMoving", 2);
+        }
+
+        else if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))) //right
+        {
+            anim.SetInteger("isMoving", 1);
+        }
+
+        else
+        {
+            anim.SetInteger("isMoving", 0);
+        }
+
         transform.Translate(hor * speed * Time.deltaTime, 0, 0);
 	}
 
