4633e231371b5f79fa7a54fc3b6f8e59e879e3dc
Harrison John Hopkins <hguyhopkins@gmail.com>
2016-08-27 14:43:53 -0400

    Added Animated Apple
    Enemy Apple Works!

--- Beat Beet Beat!/Assets/My Scripts/Apple.cs
+++ Beat Beet Beat!/Assets/My Scripts/Apple.cs
@@ -7,16 +7,23 @@ public class Apple : Enemy
 
     public bool chasing = false;
 
+    private Animator anim;
+
 	// Use this for initialization
 	void Start ()
     {
-	
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         Move();
+
+        if(!chasing)
+        {
+            anim.SetInteger("isMoving", 0);
+        }
 	}
 
     public override void Move()
@@ -24,6 +31,16 @@ public class Apple : Enemy
         if(chasing)
         {
             transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x, 0.1f), 0.02f);
+
+            if(player.transform.position.x - transform.position.x >= 0)
+            {
+                anim.SetInteger("isMoving", 1);
+            }
+
+            else if(player.transform.position.x - transform.position.x <= 0)
+            {
+                anim.SetInteger("isMoving", 2);
+            }
         }
     }
 
