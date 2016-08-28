46c6ff6b97d14a88282307044acdec853250bdc6
furrockoh <furrockoh@gmail.com>
2016-08-28 09:14:30 -0400

    DANK ME-MES
    DAAAAAANK.
    
    ARE YOU SURE ABOUT THAT?

---
+++ Beat Beet Beat!/Assets/LevelHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelHandler : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadLevel(string level)
+    {
+        transform.GetChild(0).GetComponent<Text>().text = "Loading!";
+        SceneManager.LoadScene(level);
+    }
+}
---
+++ Beat Beet Beat!/Assets/MenuHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour
+{
+    public GameObject[] menu1;
+    public GameObject[] menu2;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SwapMenu()
+    {
+        foreach (GameObject i in menu1)
+        {
+            i.SetActive(false);
+        }
+        foreach (GameObject i in menu2)
+        {
+            i.SetActive(true);
+        }
+    }
+}
