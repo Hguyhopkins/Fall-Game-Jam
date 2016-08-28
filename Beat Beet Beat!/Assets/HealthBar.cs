84ad81bb4e8d7976787c09d67efd6d9bf63be1f9
furrockoh <furrockoh@gmail.com>
2016-08-28 10:59:41 -0400

    Health
    FUCK

---
+++ Beat Beet Beat!/Assets/HealthBar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<RectTransform>().sizeDelta = new Vector2(GetComponent<RectTransform>().sizeDelta.x, 120f * (player.GetComponent<PlayerController>().health / 100f));
+        //transform.localPosition = new Vector3(transform.localPosition.x, (-130f + 10f * (player.GetComponent<PlayerController>().health / 100f)) / 130f, 0);
+	}
+}
