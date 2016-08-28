1ef284551e641e7ff6c6fde1f2cc4d3b4ae3a833
furrockoh <furrockoh@gmail.com>
2016-08-28 05:51:51 -0400

    TFW You tryin' to be boss but you can't even beat a level 2 goblin from lumbridge
    Btw, this has boss shit in it

---
+++ Beat Beet Beat!/Assets/My Scripts/Cyclapple.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cyclapple : MonoBehaviour {
+
+    public GameObject player;
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (anim.GetBool("isTalking") == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+                anim.SetBool("isTalking", true);
+        }
+        else if (anim.GetBool("readyToFight") == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+                anim.SetBool("readyToFight", true);
+        }
+        else
+        {
+            Debug.Log(player.transform.position.x - transform.position.x);
+            if (player.transform.position.x - transform.position.x > 0)
+            {
+                if (anim.GetInteger("isMoving") != 2)
+                {
+                    anim.SetInteger("isMoving", 2);
+                }
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x, 0.1f), 0.02f);
+            }
+            else if (player.transform.position.x - transform.position.x < 0)
+            {
+                if (anim.GetInteger("isMoving") != 1)
+                {
+                    anim.SetInteger("isMoving", 1);
+                }
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x, 0.1f), 0.02f);
+            }
+            else if (anim.GetInteger("isMoving") != 0)
+            {
+                anim.SetInteger("isMoving", 0);
+            }
+        }
+    }
+}
